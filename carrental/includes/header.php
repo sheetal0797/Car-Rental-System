b14dc3bd51517f5b047a11412b206d2dd6cb34e2
SheetalAgarwal <Sheetal0797@gmail.com>
2023-05-27 23:23:19 +0530

    carlease

---
+++ carrental/includes/header.php
@@ -0,0 +1,69 @@
+
+<header>
+  
+  
+  <!-- Navigation -->
+  <nav id="navigation_bar" class="navbar navbar-default">
+    <div class="container"><a href="index.php"><img src="assets/images/logo.png" alt="image"/></a>
+      <div class="navbar-header">
+        <button id="menu_slide" data-target="#navigation" aria-expanded="false" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      </div>
+      <div class="header_wrap">
+       
+		   
+            
+          
+      <div class="collapse navbar-collapse" id="navigation">
+        <ul class="nav navbar-nav">
+         <li> </li>
+		 <li><a href="index.php">Home</a>    </li>
+          	 
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+          <li><a href="car-listing.php">Car Listing</a>
+          <li><a href="page.php?type=faqs">FAQs</a></li>
+          <li><a href="contact-us.php">Contact Us</a></li>
+		  
+		  <div class="user_login">
+          <ul>
+		   <li class="dropdown"> <a href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user-circle" aria-hidden="true"></i> 
+<?php 
+$email=$_SESSION['login'];
+$sql ="SELECT FullName FROM tblusers WHERE EmailId=:email ";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+	{
+
+	 echo htmlentities($result->FullName); }}?><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+              <ul class="dropdown-menu">
+           <?php if($_SESSION['login']){?>
+            <li><a href="profile.php">Profile Settings</a></li>
+              <li><a href="update-password.php">Update Password</a></li>
+            <li><a href="my-booking.php">My Booking</a></li>
+            <li><a href="post-testimonial.php">Post a Testimonial</a></li>
+          <li><a href="my-testimonials.php">My Testimonial</a></li>
+            <li><a href="logout.php">Sign Out</a></li>
+            <?php } else { ?>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Profile Settings</a></li>
+              <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Update Password</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Booking</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Post a Testimonial</a></li>
+          <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">My Testimonial</a></li>
+            <li><a href="#loginform"  data-toggle="modal" data-dismiss="modal">Sign Out</a></li>
+            <?php } ?>
+          </ul>
+            </li></ul>
+        </div>
+		  
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- Navigation end --> 
+  
+</header>
